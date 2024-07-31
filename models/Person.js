a3d0f6781de27af7dfd402d2c3fe62bbf6280c34
Vishal-coder8400 <vktgkp7@gmail.com>
2024-07-31 14:25:48 +0530

    first commit

--- models/Person.js
+++ models/Person.js
@@ -5,30 +5,18 @@ const personSchema=new mongoose.Schema({
         type:String,
         required:true
     },
-    age:{
-        type:Number
-    },
+   
     work:{
         type:String,
-        enum:['chef','waiter','manager'],
         required:true
     },
-    mobile:{
-        type:String,
-        required:true
-    },
+   
     email:{
         type:String,
         required:true,
         unique:true
     },
-    address:{
-        type:String
-    },
-    salary:{
-        type:Number,
-        required:true
-    },
+    
 })
 
 const PersonModel=mongoose.model('Person',personSchema);
